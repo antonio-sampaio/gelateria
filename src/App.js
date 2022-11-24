6c3945fbb4704d7d2d9340f524ab1b1222b63677
antonio-sampaio <andre-s.sampaio@hotmail.com>
2022-11-24 15:54:26 -0300

    primeiro commit do arquivo gelateria

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Rotas from  '../src/paginas/rotas';
+
+
+function App() {
+  return (
+    <div className="App">
+          <Rotas/>
+    </div>
+  );
+}
+
+export default App;
